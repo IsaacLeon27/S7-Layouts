8d2ae799c4127c8066919100e739e29539f17ec6
IsaLeon <progwebdeb@gmail.com>
2020-07-21 09:17:17 -0600

    Initial commit

---
+++ app/src/main/java/com/iwalnexus/tsn/layouts/MainActivity.java
@@ -0,0 +1,14 @@
+package com.iwalnexus.tsn.layouts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
